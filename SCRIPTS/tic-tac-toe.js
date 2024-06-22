4b7474e7134b716e93fd81d08a72f2b5bc1478b3
pvn <pvnnraj@gmail.com>
2024-06-22 16:53:21 +0530

    added and confirmed win condition checking logic

--- SCRIPTS/tic-tac-toe.js
+++ SCRIPTS/tic-tac-toe.js
@@ -7,6 +7,7 @@ let boardO= [['0','0','0'], ['0','0','0'], ['0','0','0']];
 let turn= 'X';
 let row, column;
 let moveCount= 0;
+let result= ""
 /*----- cached element references -----*/
 
 
@@ -29,13 +30,19 @@ function playgame(row, column){
   moveCount++;
   if(turn=="X"){
     boardX[row][column]= 1;
+    if(moveCount>=5){
+      checkWin(boardX);
+    }
     turn= "O";
   }else{
     boardO[row][column]= 1;
+    if(moveCount>=5){
+      checkWin(boardO);
+    }
     turn= "X";
   }
-  render(boardX, "X");
-  render(boardO, "O");
+  //render(boardX, "X");
+  //render(boardO, "O");
 }
 function render(board, player){
   console.log(`This is ${player}`);
@@ -44,5 +51,31 @@ function render(board, player){
       console.log(`${board[i][j]} `);
     }
     console.log('\n')
+  }
+}
+function checkWin(board){
+  //Horizontal and Vertical Check
+  let won= false
+  for(let i=0; i<3; i++){
+    if(board[i][0]==1 && board[i][1]==1 && board[i][2]==1){
+      won= true;
+      break;
+    }
+    if(board[0][i]==1 && board[1][i]==1 && board[2][i]==1){
+      won= true;
+      break;
+    }
+  }
+  //Diagonal Check
+  if(!won){
+    if(board[0][0]==1 && board[1][1]==1 && board[2][2]==1){
+      won=true;
+    }else if(board[0][2]==1 && board[1][1]==1 && board[2][0]==1){
+      won=true;
+    }
+  }
+  if(won){
+    result= `Player ${turn} wins.`
+    console.log(result);
   }
 }
