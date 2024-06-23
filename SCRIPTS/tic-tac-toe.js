922e0a4d096d0c2e7396d35d17aa88bd8c4a26d7
pvn <pvnnraj@gmail.com>
2024-06-23 11:07:27 +0530

    added minimal css and logic for crossing out squares

--- SCRIPTS/tic-tac-toe.js
+++ SCRIPTS/tic-tac-toe.js
@@ -19,12 +19,14 @@ document.body.querySelectorAll(`.js-square`).forEach(button =>{
 
 /*----- functions -----*/
 function handleClick(event){
-  button= event.target;
-  const rows= parseInt(button.getAttribute('data-row'));
-  const columns= parseInt(button.getAttribute('data-col'));
-  displayMove(button);
-  playgame(rows, columns);
-  button.removeEventListener("click", handleClick);
+  if(result==""){
+    button= event.target;
+    const rows= parseInt(button.getAttribute('data-row'));
+    const columns= parseInt(button.getAttribute('data-col'));
+    displayMove(button);
+    playgame(rows, columns);
+    button.removeEventListener("click", handleClick);
+  }
 }
 
 function playgame(row, column){
@@ -55,15 +57,26 @@ function render(board, player){
   }
 }
 function checkWin(board){
+  let won= false;
+  let winningCombination;
   //Horizontal and Vertical Check
-  let won= false
   for(let i=0; i<3; i++){
     if(board[i][0]==1 && board[i][1]==1 && board[i][2]==1){
       won= true;
+      winningCombination=[
+        [i, 0],
+        [i, 1],
+        [i, 2]
+      ]
       break;
     }
     if(board[0][i]==1 && board[1][i]==1 && board[2][i]==1){
       won= true;
+      winningCombination=[
+        [0, i],
+        [1, i],
+        [2, i]
+      ]
       break;
     }
   }
@@ -71,16 +84,33 @@ function checkWin(board){
   if(!won){
     if(board[0][0]==1 && board[1][1]==1 && board[2][2]==1){
       won=true;
+      winningCombination=[
+        [0, 0],
+        [1, 1],
+        [2, 2]
+      ]
     }else if(board[0][2]==1 && board[1][1]==1 && board[2][0]==1){
       won=true;
+      winningCombination=[
+        [0, 2],
+        [1, 1],
+        [2, 0]
+      ]
     }
   }
   if(won){
     result= `Player ${turn} wins.`
+    highlightWinningSquares(winningCombination);
     document.body.querySelector('.result-line').innerHTML=result;
     console.log(result);
   }
 }
 function displayMove(button){
   button.innerHTML=`<img src="IMAGES\\tic-tac-toe-${turn}.png" class="css-move">`;
+}
+function highlightWinningSquares(comb){
+  comb.forEach(([row, column]) => {
+    const button= document.body.querySelector(`.js-square[data-row="${row}"][data-col="${column}"]`);
+    button.classList.add('winning-square');
+  })
 }
